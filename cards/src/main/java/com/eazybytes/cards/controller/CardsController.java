1ec3e9e4bdbd54c80ad6831f8de97d0abe1a00c6
phong.bnh <phong.bnh@globalfactories.com>
2023-09-08 21:19:48 +0700

    feat: update cards service

--- cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -1,11 +1,17 @@
 /**
- * 
+ *
  */
 package com.eazybytes.cards.controller;
 
 import java.util.List;
 
+import com.eazybytes.cards.config.CardsServiceConfig;
+import com.eazybytes.cards.model.Properties;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,24 +22,34 @@ import com.eazybytes.cards.repository.CardsRepository;
 
 /**
  * @author Eazy Bytes
- *
  */
 
 @RestController
 public class CardsController {
 
-	@Autowired
-	private CardsRepository cardsRepository;
+    @Autowired
+    private CardsRepository cardsRepository;
 
-	@PostMapping("/myCards")
-	public List<Cards> getCardDetails(@RequestBody Customer customer) {
-		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
-		if (cards != null) {
-			return cards;
-		} else {
-			return null;
-		}
+    @Autowired
+    private CardsServiceConfig cardsServiceConfig;
 
-	}
+    @PostMapping("/myCards")
+    public List<Cards> getCardDetails(@RequestBody Customer customer) {
+        List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+        if (cards != null) {
+            return cards;
+        } else {
+            return null;
+        }
+
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsServiceConfig.getMsg(), cardsServiceConfig.getBuildVersion(), cardsServiceConfig.getMailDetails(), cardsServiceConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
+    }
 
 }
